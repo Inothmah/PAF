9e0c98baa9e92ad0c1d66dbaa5366017a1807f0f
Chandrarathna A A U M <it23603936@my.sliit.lk>
2026-04-18 19:50:59 +0530

    feat(backend): add resource inventory enums

---
+++ demo/src/main/java/com/group/smartcampus/demo/model/ResourceStatus.java
@@ -0,0 +1,8 @@
+package com.group.smartcampus.demo.model;
+
+public enum ResourceStatus {
+    ACTIVE,
+    OUT_OF_SERVICE,
+    MAINTENANCE,
+    RESERVED
+}
---
+++ demo/src/main/java/com/group/smartcampus/demo/model/ResourceType.java
@@ -0,0 +1,12 @@
+package com.group.smartcampus.demo.model;
+
+public enum ResourceType {
+    LECTURE_HALL,
+    LABORATORY,
+    MEETING_ROOM,
+    CLASSROOM,
+    LIBRARY,
+    AUDITORIUM,
+    COMPUTER_LAB,
+    EQUIPMENT
+}
